a28f4daa90bb36c8a4d266af2f422052399fdecf
rtavares <ricardotavaresit@gmail.com>
2022-06-14 17:15:05 +0100

    create search-component

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="onSubmmit">
+      <input type="text" placeholder="CEP" v-model="cep"/>
+      <button type="submit" >Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hi",
+      cep: "",
+    };
+  },
+  methods: {
+    onSubmmit() {
+      alert("oi" + this.cep);
+    },
+  },
+};
+</script>
