d75b0e42e1aa666f6fe1326cb7db5c060959884d
rtavares <ricardotavaresit@gmail.com>
2022-06-14 17:36:17 +0100

    create pre loader

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -4,6 +4,9 @@
       <input type="text" placeholder="CEP" v-model="cep" />
       <button type="submit">Search</button>
     </form>
+    <div v-if="preLoader">
+      <img class="pre-loader" src="./../assets/Swing-Preloader.svg" />
+    </div>
     <div v-if="adress.cep">{{ adress }}</div>
   </div>
 </template>
@@ -15,18 +18,28 @@ export default {
     return {
       cep: "",
       adress: {},
+      preLoader: false,
     };
   },
   methods: {
     onSubmmit() {
+      this.preLoader = true;
       axios
         .get(`https://viacep.com.br/ws/${this.cep}/json`)
         .then((response) => {
           this.adress = response.data;
           console.log(this.adress);
         })
-        .catch((error) => console.error(error));
+        .catch((error) => console.error(error))
+        .finally(() => (this.preLoader = false));
     },
   },
 };
 </script>
+
+
+<style scoped>
+.pre-loader {
+  max-width: 100px;
+}
+</style>
